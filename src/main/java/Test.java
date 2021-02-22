3eda641bcfd14f425fb777005b49b2262d60778a
xyc <xuyaochang@126.com>
2021-02-22 21:14:55 +0800

    分支dev1添加一条语句，共3条

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("idea分支Git_Test项目测试");
         System.out.println("添加一条语句，共2条");
+        System.out.println("分支dev1添加一条语句，共3条");
     }
 }

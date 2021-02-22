c0c4091f4b9a4d5083da7cb24269764e4a2a6bcd
xyc <xuyaochang@126.com>
2021-02-22 20:54:27 +0800

    新创建，添加一条sout语句

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author: ASD
+ * @date: 2021/2/22 20:50:52
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("idea分支Git_Test项目测试");
+    }
+}
